aef5feefeea2bd9c535ab8672e5054386eaa47b2
Ashwin kumar Venkatesan <ashwinvenkat14@gmail.com>
2024-08-01 17:31:05 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function toggleMenu(params) {
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hambburger-icon");
+    menu.classList.toggle("open")
+    icon.classList.toggle("open")
+
+}
